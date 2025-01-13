7a064800f170ccc0586caf3b0e4be30af1a6ed68
Vince042894 <carasketonlinejob@gmail.com>
2025-01-13 15:40:27 +0800

    create data base and input data using django admin

--- restaurantmenu/admin.py
+++ restaurantmenu/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+admin.site.site_header = "Yummy Philippines Administration"
+admin.site.site_title = "Yummy Philippines Admin Portal"
+admin.site.index_title = "Welcome to the Yummy Philippines Admin Panel"
+
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status")
+    list_filter = ("status", )
+    search_fields = ("meal", "description")
+
+
+admin.site.register(Item, MenuItemAdmin)
--- restaurantmenu/views.py
+++ restaurantmenu/views.py
@@ -1,3 +1,4 @@
+from django.utils.translation.template import context_re
 from django.views import generic
 from django.views.generic import DetailView
 
@@ -6,6 +7,11 @@ from .models import Item, MEAL_TYPE
 class Menulist(generic.ListView):
     queryset = Item.objects.order_by("date_created")
     template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = MEAL_TYPE
+        return context
 
 
 class MenuItemDetail(DetailView):

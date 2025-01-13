a260fa5631cf7be788e043671a75616f6d871f4f
Vince042894 <carasketonlinejob@gmail.com>
2025-01-13 17:31:39 +0800

    html indentation

--- restaurantmenu/urls.py
+++ restaurantmenu/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('',views.Menulist.as_view(), name='home'), #home
-    path('', views.MenuItemDetail.as_view(), name="menu_item")
+    path('',views.Menulist.as_view(), name='home'),
 ]
